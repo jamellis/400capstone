4ab8e9be4c4fa11b2e32a7a9e8ff1a8dbf76653e
james ellis <jellis@in-tek.net>
2012-08-12 16:27:20 -0700

    Updated Report Form size and buttons.

--- vb/NorthwesternTiresCompany/frmReport.Designer.vb
+++ vb/NorthwesternTiresCompany/frmReport.Designer.vb
@@ -32,34 +32,39 @@ Partial Class frmReport
         CType(Me.SalesReportTableAdapterBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-
+        'SalesReportTableAdapterBindingSource
+        '
         Me.SalesReportTableAdapterBindingSource.DataMember = "SalesReportTableAdapter"
-
+        '
+        'lblReports
+        '
         Me.lblReports.AutoSize = True
         Me.lblReports.Font = New System.Drawing.Font("Arial", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblReports.Location = New System.Drawing.Point(296, 16)
-        Me.lblReports.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblReports.Location = New System.Drawing.Point(240, 20)
         Me.lblReports.Name = "lblReports"
-        Me.lblReports.Size = New System.Drawing.Size(93, 26)
+        Me.lblReports.Size = New System.Drawing.Size(120, 33)
         Me.lblReports.TabIndex = 0
         Me.lblReports.Text = "Reports"
-
         '
-        Me.btnSalesReport.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnSalesReport.Location = New System.Drawing.Point(113, 103)
-        Me.btnSalesReport.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        'btnSalesReport
+        '
+        Me.btnSalesReport.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalesReport.Location = New System.Drawing.Point(224, 127)
+        Me.btnSalesReport.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnSalesReport.Name = "btnSalesReport"
-        Me.btnSalesReport.Size = New System.Drawing.Size(160, 38)
+        Me.btnSalesReport.Size = New System.Drawing.Size(149, 37)
         Me.btnSalesReport.TabIndex = 1
         Me.btnSalesReport.Text = "Sales Report"
         Me.btnSalesReport.UseVisualStyleBackColor = True
-
+        '
+        'btnExit
         '
         Me.btnExit.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnExit.ForeColor = System.Drawing.Color.Red
-        Me.btnExit.Location = New System.Drawing.Point(582, 470)
+        Me.btnExit.Location = New System.Drawing.Point(428, 338)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(90, 30)
+        Me.btnExit.Size = New System.Drawing.Size(120, 37)
         Me.btnExit.TabIndex = 15
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
@@ -68,29 +73,30 @@ Partial Class frmReport
         '
         Me.btnBack.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnBack.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.btnBack.Location = New System.Drawing.Point(12, 470)
+        Me.btnBack.Location = New System.Drawing.Point(48, 338)
+        Me.btnBack.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnBack.Name = "btnBack"
-        Me.btnBack.Size = New System.Drawing.Size(90, 30)
+        Me.btnBack.Size = New System.Drawing.Size(120, 37)
         Me.btnBack.TabIndex = 14
         Me.btnBack.Text = "&Back"
         Me.btnBack.UseVisualStyleBackColor = True
         '
         'btnInventoryReport
         '
-        Me.btnInventoryReport.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnInventoryReport.Location = New System.Drawing.Point(113, 162)
-        Me.btnInventoryReport.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnInventoryReport.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInventoryReport.Location = New System.Drawing.Point(224, 199)
+        Me.btnInventoryReport.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.btnInventoryReport.Name = "btnInventoryReport"
-        Me.btnInventoryReport.Size = New System.Drawing.Size(160, 41)
+        Me.btnInventoryReport.Size = New System.Drawing.Size(149, 37)
         Me.btnInventoryReport.TabIndex = 16
         Me.btnInventoryReport.Text = "Inventory Report"
         Me.btnInventoryReport.UseVisualStyleBackColor = True
         '
         'frmReport
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(684, 512)
+        Me.ClientSize = New System.Drawing.Size(597, 410)
         Me.ControlBox = False
         Me.Controls.Add(Me.btnInventoryReport)
         Me.Controls.Add(Me.btnExit)
@@ -98,14 +104,13 @@ Partial Class frmReport
         Me.Controls.Add(Me.btnSalesReport)
         Me.Controls.Add(Me.lblReports)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmReport"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Reports"
         CType(Me.SalesReportTableAdapterBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        'CType(Me.comp400_2012DataSetSalesReporting, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
