a3956ecb0a93712b811254684b577eab1ee11b1e
james ellis <jellis@in-tek.net>
2012-08-12 13:51:43 -0700

    Inventory Report Completed

--- vb/NorthwesternTiresCompany/My Project/Application.Designer.vb
+++ vb/NorthwesternTiresCompany/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.NorthwesternTiresCompany.frmLogin
+            Me.MainForm = Global.NorthwesternTiresCompany.frmReportInventoryReport
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
